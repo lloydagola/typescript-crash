beb41fb23e9cec8d8321ade023127fa755c2129a
Lord Lloyd <lordlloyd@Lords-MacBook-Air.local>
2022-09-20 18:05:35 +0300

    implemented edit function

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TodoModel } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -11,32 +11,77 @@ interface Props {
 }
 
 const Todo: React.FC<Props> = ({ todo, todos, setTodos, key }) => {
-  const handleDone = (id: number) =>
-    setTodos(
-      todos.map((todo) =>
+  const [editing, setEditing] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+
+  const handleDoneClick = (id: number) =>
+    setTodos((previousTodos) =>
+      previousTodos.map((todo) =>
         todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
       )
     );
 
-  const handleDelete = (id: number) =>
-    setTodos(todos.filter((current) => current.id !== todo.id));
-
-  return (
-    <form className="todos__single">
-      {todo.isDone ? (
+  const handleDone = () => {
+    if (editing) {
+      return (
+        <input
+          value={value}
+          onChange={(e) =>
+            setValue((currentValue) =>
+              currentValue !== e.target.value ? e.target.value : currentValue
+            )
+          }
+        />
+      );
+    } else {
+      return todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
       ) : (
         <span className="todos__single--text">{todo.todo}</span>
-      )}
+      );
+    }
+  };
+
+  const handleEditClick = (id: number) =>
+    setTodos((previousTodos) =>
+      previousTodos.map((current) => {
+        if (current.id === id) {
+          setEditing(!editing);
+        }
+        return current;
+      })
+    );
+  const handleDeleteClick = (id: number) =>
+    setTodos((previousTodos) =>
+      previousTodos.filter((current) => current.id !== todo.id)
+    );
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos((previousTodos) =>
+      previousTodos.map((current) =>
+        current.id === todo.id ? { ...current, todo: value } : current
+      )
+    );
+    setEditing(false);
+  };
+
+  useEffect(() => {
+    setValue(todo.todo);
+  }, [todo, todos, value]);
+
+  return (
+    <form className="todos__single" onSubmit={handleFormSubmit}>
+      {handleDone()}
       <div>
         <span className="icon">
-          <AiFillEdit />
+          <AiFillEdit onClick={() => handleEditClick(todo.id)} />
         </span>
         <span className="icon">
-          <AiFillDelete onClick={() => handleDelete(todo.id)} />
+          <AiFillDelete onClick={() => handleDeleteClick(todo.id)} />
         </span>
         <span className="icon">
-          <MdDone onClick={() => handleDone(todo.id)} />
+          <MdDone onClick={() => handleDoneClick(todo.id)} />
         </span>
       </div>
     </form>
